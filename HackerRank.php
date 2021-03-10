b9041cc83797cc17829411bcf58bfa6b4ba79869
Wambulwa Ayub <sikuku.wambulwa@strathmore.edu>
2021-03-10 16:42:10 +0300

    Create HackerRank.php

---
+++ HackerRank.php
@@ -0,0 +1,20 @@
+<?php
+//Sales by Match
+function sockMerchant($n, $ar) {
+    $n = count($ar);
+    $new_arr=array_count_values($ar);
+    $k=0;
+    $k1=0;
+    foreach($new_arr as $key=>$value){
+        if($value == 2){
+            $k += 1;
+        }
+        if($value>2){
+            $k1 +=intdiv($value,2);
+        }
+    }
+    return $k+$k1;
+}
+//Delete Below Code before running. It is for testing purposes
+// 
+echo sockMerchant(11,[1,2,1,3,4,5,6,7,2,9,0]);
